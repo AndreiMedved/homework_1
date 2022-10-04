32e9885debadddf39370970942a62527cf6fc7f9
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 22:37:13 +0300

    finished  task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,7 @@
+﻿// Task_6 Показать последнюю цифру трёхзначного числа
+int a = new Random().Next(100, 1000);
+string number = Convert.ToString(a);
+char last = number[2];
+Console.WriteLine(a);
+Console.WriteLine(last);
+
