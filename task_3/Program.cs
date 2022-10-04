eec587fee6ec31f8b10b166d2fbb8ced97ff4763
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 22:11:15 +0300

    finished  task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task_3 Найти максимальное из трех чисел
+int a = new Random().Next(0, 100), b = new Random().Next(0, 100), c = new Random().Next(0, 100);
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("largest among numbers " + a + ", " + b + ", " + c + " is " + max);
+
