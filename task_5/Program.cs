d721479c02a53bd5f30c4c10909e4a6485351b22
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 22:26:31 +0300

    finished  task_5

---
+++ task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task_5 Показать четные числа от 1 до N
+int count = 1;
+int n = new Random().Next(0, 10);
+
+Console.WriteLine(n);
+
+while(n >= count)
+{
+    Console.Write(count + " ");
+    count++;
+}
+
