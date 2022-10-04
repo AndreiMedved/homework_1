7982221fe4ab260c975fdf978d9d1d4f5302cea8
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-04 22:17:31 +0300

    finished  task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task_4 Выяснить является ли число чётным
+int a = new Random().Next(0, 10);
+
+Console.WriteLine(a);
+if(a%2 == 0)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
