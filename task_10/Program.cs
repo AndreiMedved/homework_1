68603f743b1e5a4d2b065d8a43cd129f75eeae68
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-05 19:59:25 +0300

    finished  task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task_10 Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int a = new Random().Next(11, 100);
+int div = new Random().Next(0, 10);
+
+Console.WriteLine(a + " / " + div);
+
+if(a%div == 0)
+{
+    Console.WriteLine("without remainder");
+}
+else
+{
+    Console.WriteLine("remainder " + a%div);
+}
