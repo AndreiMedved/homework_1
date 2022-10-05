53eaf4a67395ab79dd42e3ed872d39e01adb932c
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-05 10:00:31 +0300

    finished  task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task_8 Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 100);
+string number = Convert.ToString(a);
+char first = number[0];
+char second = number[1];
+Console.WriteLine(a);
+
+if(first > second)
+{
+    Console.WriteLine(first);
+}
+if(second > first)
+{
+    Console.WriteLine(second);
+}
+else
+{
+    Console.WriteLine("similar numbers");
+}
