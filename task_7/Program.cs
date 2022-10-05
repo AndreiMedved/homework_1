415d7e903b4ded7c01b7d08804b0a4f3e62066bd
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-05 09:28:51 +0300

    finished  task_7

---
+++ task_7/Program.cs
@@ -0,0 +1,6 @@
+﻿// Task_7 Показать вторую цифру трёхзначного числа
+int a = new Random().Next(100, 1000);
+string number = Convert.ToString(a);
+char second = number[1];
+Console.WriteLine(a);
+Console.WriteLine(second);
