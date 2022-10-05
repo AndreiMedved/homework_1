91d6ad1700db705319fb9f414232b296c7ebef7a
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-05 19:48:13 +0300

    finished  task_9

---
+++ task_9/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task_9 Удалить вторую цифру трёхзначного числа
+int a = new Random().Next(100, 1000);
+String number = Convert.ToString(a);
+char n0 = number[0];
+char n1 = number[2];
+Console.WriteLine(a);
+Console.WriteLine((n0.ToString()) + (n1.ToString()));
+
